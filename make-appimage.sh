013e9afbd0265f0a2339a0f160a3c4645e0b1659
Link4Electronics <link4electronics@gmail.com>
2026-03-29 13:02:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,11 +6,13 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q scummvm | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.scummvm.scummvm.svg
 export DESKTOP=/usr/share/applications/org.scummvm.scummvm.desktop
 export DEPLOY_OPENGL=1
+export DEPLOY_GTK=1
+export GTK_DIR=gtk-3.0
 
 # Deploy dependencies
 quick-sharun /usr/bin/scummvm
