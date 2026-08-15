7327f84d36da448fd62dcfa0786f8188c38b5bf5
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:56:22 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q scummvm | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q scummvm | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.scummvm.scummvm.svg
 export DESKTOP=/usr/share/applications/org.scummvm.scummvm.desktop
-export DEPLOY_OPENGL=1
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 export DEPLOY_PIPEWIRE=1 # needed for libfluidsynth
 
 # Deploy dependencies
 quick-sharun /usr/bin/scummvm
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
