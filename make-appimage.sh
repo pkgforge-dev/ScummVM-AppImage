79d68b8a1db6e8ef77b54954a740763444533c37
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:56:22 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q scummvm | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q scummvm | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -17,8 +17,6 @@ export DEPLOY_PIPEWIRE=1 # needed for libfluidsynth
 
 # Deploy dependencies
 quick-sharun /usr/bin/scummvm
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
